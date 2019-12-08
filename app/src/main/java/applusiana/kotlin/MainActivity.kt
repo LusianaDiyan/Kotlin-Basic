d6849b7604efe8ec5c39e6f951cd5105c5d3f05c
LusianaDiyan <lusianadiyan25@gmail.com>
2019-12-08 10:52:12 +0700

    first commit

---
+++ app/src/main/java/applusiana/kotlin/MainActivity.kt
@@ -0,0 +1,20 @@
+package applusiana.kotlin
+
+import android.content.Intent
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var button : Button
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        button = findViewById<Button>(R.id.button)
+        button.setOnClickListener{
+            startActivity(Intent(this, SecondActivity::class.java))
+        }
+    }
+}
